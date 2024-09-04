bc1568a20051bb5bba618e3df095f7f18de9ced3
Spring <3171763596@qq.com>
2024-09-04 14:29:36 +0800

    1

--- src/main/java/cn/njcit/controller/UserController.java
+++ src/main/java/cn/njcit/controller/UserController.java
@@ -1,7 +1,15 @@
 package cn.njcit.controller;
 
+import cn.njcit.entity.User;
+import cn.njcit.service.IClassService;
+import cn.njcit.service.IUserService;
+import cn.njcit.util.ResponseResult;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * <p>
@@ -12,7 +20,16 @@ import org.springframework.stereotype.Controller;
  * @since 2024-09-03
  */
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/studentManage")
 public class UserController {
-
+    @Autowired
+    private IUserService userService;
+    @Autowired
+    private IClassService classService;
+    @GetMapping("/list")
+    public ResponseResult studentManage(@RequestParam(defaultValue = "1")Integer page,
+                                        @RequestParam(required = false) String searchName){
+        PageInfo<User> stuentList=userService.getStudentList(page,searchName);
+        return ResponseResult.ok().put("studentList",stuentList);
+    }
 }
